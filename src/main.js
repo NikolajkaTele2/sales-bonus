0b6542de65b3658795d55a59dd71874a55beb8b2
Nick Bystrov <projectuniver@yandex.ru>
2025-07-17 03:12:59 +0300

    toTheMoon9

--- src/main.js
+++ src/main.js
@@ -91,7 +91,8 @@ function analyzeSalesData(data, options) {
 
     sellerStats.forEach((seller, index) => {
         seller.bonus = calculateBonusByProfit(index, sellerStats.length, seller) * seller.profit / 1000
-        seller.top_products = Object.entries(seller.products_sold).sort((a, b) => b[1] - a[1]).slice(0, 10).map(([sku, quantity]) => [sku, quantity]);
+        seller.top_products = Object.entries(seller.products_sold).sort((a, b) => b[1] - a[1]).slice(0, 10);
+        console.log(seller.top_products)
     });
     return sellerStats.map(seller => ({
         seller_id: seller.seller_id,
